7225aa84dc2b50f73a131d7702eaa6940447d596
ndfriedman <mail4noahf@gmail.com>
2017-11-15 12:14:22 -0800

    wrote code to download workbooks by reading in 'variantData'

--- downloader.js
+++ downloader.js
@@ -6,11 +6,44 @@ function s2ab(s) {
   return buf;
 }
 
-function downloadDataAsXls(workbook){
-	console.log('gendarme');
-	var wopts = { bookType:'xlsx', bookSST:false, type:'binary' };
+//parses our variant data construct and returns a set of headers and cell data
+function convertVariantDataToWorkbook(vData){
+	if (typeof vData[0] == 'undefined'){ //in case we call this function to early return
+		console.log('issues');
+		return 
+	}
+	arr = []
+	for(var i in vData){
+		row = {}
+		for(var col in vData[i]['core']){
+			row[col] = vData[i]['core'][col]['originalValue'] //should it be the variable original value
+		}
+		arr.push(row)
+	}
+	var cols = [] //get the columns for the spreadsheet
+	for(i in arr[0]){
+		cols.push(i)
+	}
+	return [arr, cols]
+}
 
-	var wbout = XLSX.write(workbook,wopts);
-	/* the saveAs call downloads a file on the local machine */
+function downloadDataAsXls(vData){
+	var wb = { SheetNames:[], Sheets:{} };
+	
+	var dataForXls = convertVariantDataToWorkbook(vData);
+	if (typeof dataForXls == 'undefined'){
+		console.log('unable to download')
+		return 
+	}
+	//prepare for xls, then write with boilerplate
+	var cells = dataForXls[0];
+	var cols = dataForXls[1];
+	console.log(cells)
+	wb.SheetNames.push('Sheet1');
+	var ws = XLSX.utils.json_to_sheet(cells, {header:cols});
+	wb['Sheets']['Sheet1'] = ws
+	var wopts = { bookType:'xlsx', bookSST:false, type:'binary' };
+	var wbout = XLSX.write(wb,wopts);
+	// the saveAs call downloads a file on the local machine 
 	saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "noahTest.xlsx");
 }
